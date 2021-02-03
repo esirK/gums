78e892a64fa33033f03f3d44a90adbcbbc2377be
esir <esirkings@gmail.com>
2021-02-04 02:48:15 +0300

    Send load sensor weight in float format

---
+++ load_data_sender/load_data_sender.ino
@@ -0,0 +1,166 @@
+#include <SoftwareSerial.h>
+#include "SIM800L.h"
+
+#include "HX711.h"
+
+#define LOADCELL_DOUT_PIN  3
+#define LOADCELL_SCK_PIN  2
+#define calibration_factor -7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define SIM800_RX_PIN 10
+#define SIM800_TX_PIN 11
+#define SIM800_RST_PIN 6
+
+const char APN[] = "safaricom,saf,data";
+const char URL[] = "http://c0eafc8d79c0.ngrok.io/v1/sensors/";
+const char CONTENT_TYPE[] = "application/json";
+
+SIM800L* sim800l;
+
+HX711 scale;
+
+void setup() {
+  // Initialize Serial Monitor for debugging
+  Serial.begin(115200);
+  while(!Serial);
+
+  // Initialize a SoftwareSerial
+  SoftwareSerial* serial = new SoftwareSerial(SIM800_RX_PIN, SIM800_TX_PIN);
+  Serial.println("Initializing Serial Monitor...");
+  serial->begin(9600);
+  delay(1000);
+
+  // Initialize SIM800L driver with an internal buffer of 200 bytes and a reception buffer of 512 bytes, debug disabled
+  sim800l = new SIM800L((Stream *)serial, SIM800_RST_PIN, 200, 512);
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  Serial.println("Calibrating scale...");
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  // Setup module for GPRS communication
+  setupModule();
+}
+ 
+void loop() {
+  // Establish GPRS connectivity (5 trials)
+  bool connected = false;
+  for(uint8_t i = 0; i < 5 && !connected; i++) {
+    delay(1000);
+    connected = sim800l->connectGPRS();
+  }
+
+  // Check if connected, if not reset the module and setup the config again
+  if(connected) {
+    Serial.println(F("connected !"));
+  } else {
+    Serial.println(F("Reset the module."));
+    sim800l->reset();
+    setupModule();
+  }
+
+  Serial.println(F("Start HTTP POST..."));
+  
+  float i = scale.get_units();
+  Serial.print("Load_cell output val: ");
+  Serial.println(i);
+  char PAYLOAD[200];
+  char temp_weight[6];
+  dtostrf(i, 5, 2, temp_weight);
+
+  sprintf(PAYLOAD, "{\"weight\": \"%s\", \"owner\": \"Esir\"}", temp_weight);
+
+  Serial.println(PAYLOAD);
+  // Do HTTP POST communication with 10s for the timeout (read and write)
+  uint16_t rc = sim800l->doPost(URL, CONTENT_TYPE, PAYLOAD, 10000, 10000);
+   if(rc == 200) {
+    // Success, output the data received on the serial
+    Serial.print(F("HTTP POST successful ("));
+    Serial.print(sim800l->getDataSizeReceived());
+    Serial.println(F(" bytes)"));
+    Serial.print(F("Received : "));
+    Serial.println(sim800l->getDataReceived());
+  } else {
+    // Failed...
+    Serial.print(F("HTTP POST error "));
+    Serial.println(rc);
+  }
+  // Close GPRS connectivity (5 trials)
+  bool disconnected = sim800l->disconnectGPRS();
+  for(uint8_t i = 0; i < 5 && !connected; i++) {
+    delay(1000);
+    disconnected = sim800l->disconnectGPRS();
+  }
+  
+  if(disconnected) {
+    Serial.println(F("GPRS disconnected !"));
+  } else {
+    Serial.println(F("GPRS still connected !"));
+  }
+
+  // Go into low power mode
+  bool lowPowerMode = sim800l->setPowerMode(MINIMUM);
+  if(lowPowerMode) {
+    Serial.println(F("Module in low power mode"));
+  } else {
+    Serial.println(F("Failed to switch module to low power mode"));
+  }
+
+  // Sleep for 1 minute
+  delay(60000);
+}
+
+void setupModule() {
+  Serial.println(F("Setting up GPRS..."));
+  // Establish GPRS connectivity (5 trials)
+  bool connected = false;
+  for(uint8_t i = 0; i < 5 && !connected; i++) {
+    delay(1000);
+    connected = sim800l->connectGPRS();
+  }
+
+  // Check if connected, if not reset the module and setup the config again
+  if(connected) {
+    Serial.println(F("GPRS connected !"));
+  } else {
+    Serial.println(F("GPRS not connected !"));
+    Serial.println(F("Reset the module."));
+    sim800l->reset();
+    setupModule();
+    return;
+  }
+    // Wait until the module is ready to accept AT commands
+  while(!sim800l->isReady()) {
+    Serial.println(F("Problem to initialize AT command, retry in 1 sec"));
+    delay(1000);
+  }
+  Serial.println(F("GPRS Setup Complete!"));
+
+  // Wait for the GSM signal
+  uint8_t signal = sim800l->getSignal();
+  while(signal <= 0) {
+    delay(1000);
+    signal = sim800l->getSignal();
+  }
+  Serial.print(F("Signal OK (strenght: "));
+  Serial.println(F(")"));
+  delay(1000);
+
+  // Wait for operator network registration (national or roaming network)
+  NetworkRegistration network = sim800l->getRegistrationStatus();
+  while(network != REGISTERED_HOME && network != REGISTERED_ROAMING) {
+    Serial.print(network);
+    delay(1000);
+    network = sim800l->getRegistrationStatus();
+  }
+  Serial.println(F("Network registration OK"));
+  delay(1000);
+
+  // Setup APN for GPRS configuration
+  bool success = sim800l->setupGPRS(APN);
+  while(!success) {
+    success = sim800l->setupGPRS(APN);
+    delay(5000);
+  }
+  Serial.println(F("GPRS config OK"));
+}
